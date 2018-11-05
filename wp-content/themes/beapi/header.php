41306f396c67801a62dd6c2b76f9665f0fbc6e3d
Stéphane <stephane.gillot@netapsys.fr>
2018-11-05 16:34:50 +0100

    ajout du header et footer

---
+++ wp-content/themes/beapi/header.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Be Api
+ */
+?>
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="ie" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+	<!-- title -->
+	<meta charset="utf-8">
+	<title>Vincent - Personal Portfolio Template</title>
+	
+	<!-- description -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<meta name="author" content="Pixelshow">
+	<meta name="description" content="Personal Portfolio Template">
+	
+	<!-- favicon -->
+	<link rel="shortcut icon" href="images/favicon.png" type="image/png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/apple-touch-icon-57x57-precomposed.png">
+	
+	<!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet">
+
+	<!-- styles -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+	 <!-- Javascript libraries -->
+	<script type="text/javascript" src="js/modernizr.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+		
+	<!--[if IE]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
