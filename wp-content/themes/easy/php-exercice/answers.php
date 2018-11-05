12d25874f93772dcb6da18f34f3652e923634016
Stéphane <stephane.gillot@netapsys.fr>
2018-11-05 15:06:45 +0100

    responses to the display-php test

--- wp-content/themes/easy/php-exercice/answers.php
+++ wp-content/themes/easy/php-exercice/answers.php
@@ -1,1 +1,28 @@
 <?php
+
+
+// Display - 1
+
+echo 'test echo';
+
+echo 'whatever numbers 12 12  12 ';
+
+$n = 12;
+echo " le nombre est égal à $n" ;
+
+echo '12';
+
+echo ' i\'ll echo \'special characters\' ';
+
+$condition =  ($n == 12) ? "n est égal à 12 " : " n n'est pas égal à 12";
+
+echo  ' test echo ' . " le nombre est égal à $n" . $condition;
+
+
+
+
+
+
+
+
+
